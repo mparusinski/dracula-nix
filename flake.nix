4a9f7caefa017591a38bfa7f7c963017b1fc1e45
Michal Parusinski <michal@parusinski.me>
2025-08-10 11:56:42 +0200

    refactor: formatting flake.nix

--- flake.nix
+++ flake.nix
@@ -7,86 +7,98 @@
     flake-compat.url = "github:edolstra/flake-compat";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-  let
-    inherit (nixpkgs) lib;
-    systems = lib.systems.flakeExposed;
-    devSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-    forAllSystems = lib.genAttrs systems;
-    forAllDevSystems = lib.genAttrs devSystems;
-    mkModule = { name ? "dracula", type, file }:{pkgs, ... }:{
-      _file = "${self.outPath}/flake.nix#${type}Modules.${name}";
-      imports = [ file ];
-    };
-  in {
-    checks = forAllSystems (system: {
-      pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-        src = ./.;
-        hooks = {
-          nixfmt-rfc-style.enable = true;
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      inherit (nixpkgs) lib;
+      systems = lib.systems.flakeExposed;
+      devSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = lib.genAttrs systems;
+      forAllDevSystems = lib.genAttrs devSystems;
+      mkModule =
+        {
+          name ? "dracula",
+          type,
+          file,
+        }:
+        { pkgs, ... }:
+        {
+          _file = "${self.outPath}/flake.nix#${type}Modules.${name}";
+          imports = [ file ];
         };
-      };
-    });
-    devShells = forAllDevSystems (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            nixfmt-rfc-style
-          ];
-          shellHook = ''
-            echo "Welcome to dracula-nix development shell"
-          '';
-        };
-      }
-    );
-    formatter = forAllDevSystems (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      pkgs.treefmt.withConfig {
-        runtimeInputs = with pkgs; [
-          keep-sorted
-          nixfmt-rfc-style
-        ];
-
-        settings = {
-          on-unmatched = "info";
-          tree-root-file = "flake.nix";
-
-          formatter = {
-            keep-sorted = {
-              command = "keep-sorted";
-              includes = [ "*" ];
-            };
-            nixfmt = {
-              command = "nixfmt";
-              includes = [ "*.nix" ];
-            };
+    in
+    {
+      checks = forAllSystems (system: {
+        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixfmt-rfc-style.enable = true;
+            convco.enable = true;
           };
         };
-      }
-    );
-    homeModules = {
-      default = self.homeModules.dracula;
-      dracula = mkModule {
-        type = "homeManager";
-        file = ./modules/home-manager;
+      });
+      devShells = forAllDevSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nixfmt-rfc-style
+            ];
+            shellHook = ''
+              echo "Welcome to dracula-nix development shell"
+            '';
+          };
+        }
+      );
+      formatter = forAllDevSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        pkgs.treefmt.withConfig {
+          runtimeInputs = with pkgs; [
+            keep-sorted
+            nixfmt-rfc-style
+          ];
+
+          settings = {
+            on-unmatched = "info";
+            tree-root-file = "flake.nix";
+
+            formatter = {
+              keep-sorted = {
+                command = "keep-sorted";
+                includes = [ "*" ];
+              };
+              nixfmt = {
+                command = "nixfmt";
+                includes = [ "*.nix" ];
+              };
+            };
+          };
+        }
+      );
+      homeModules = {
+        default = self.homeModules.dracula;
+        dracula = mkModule {
+          type = "homeManager";
+          file = ./modules/home-manager;
+        };
+      };
+      nixosModules = {
+        default = self.nixosModules.dracula;
+        dracula = mkModule {
+          type = "nixos";
+          file = ./modules/nixos;
+        };
       };
     };
-    nixosModules = {
-      default = self.nixosModules.dracula;
-      dracula = mkModule {
-        type = "nixos";
-        file = ./modules/nixos;
-      };
-    };
-  };
 }
